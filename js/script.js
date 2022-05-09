e2e4419acc4f736fcbfeb4e0e577d7cf8da9d581
Jiho Kim <jihokim@Jihos-MacBook-Pro.local>
2022-05-08 21:16:30 -0400

    wip: added some functionality to ADD SKILL button

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function (evt) {
+    const li = document.createElement("li");
+    const input = document.querySelector("input");
+  li.textContent = input.value;
+  document.querySelector('ul').appendChild(li);
+});
+
