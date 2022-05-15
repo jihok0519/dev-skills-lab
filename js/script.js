a5ba994d375f3c73b03654786281b5fbf54ddb2d
Jiho Kim <jihok0519@gmail.com>
2022-05-15 13:28:31 -0400

    WIP: button displays skills, breaks after removing it

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-const $btn = $("#submit");
-const $input = 
+const $btn = $(".submit");
+const $ul = $("#listedSkills");
+const $input = $("#skills");
+const $form = $("form");
+
+$ul.on("click", "button", function (event) {
+    event.preventDefault();
+    $(this).closest($ul).fadeOut(1000, function (event) {
+        $(this).remove();
+    });
+});
 
 function addInfo(event) {
-
-}
-
+    event.preventDefault();
+    let startingInput = $input.val();
+    let userInput = $ul.append(startingInput).append("<button id='remove'>x</button>");
+};
 
 $btn.on("click", addInfo);
+$form.on("button", addInfo);
