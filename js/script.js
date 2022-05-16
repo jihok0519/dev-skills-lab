81ec95ca85eb12ac7be49e1d92dc4ddd17b7b468
Jiho Kim <jihok0519@gmail.com>
2022-05-15 21:29:35 -0400

    button now adds input to new li elements

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@ const $btn = $(".submit");
 const $ul = $("#listedSkills");
 const $input = $("#skills");
 const $form = $("form");
-
-$ul.on("click", "button", function (event) {
-    event.preventDefault();
-    $(this).closest($ul).fadeOut(1000, function (event) {
-        $(this).remove();
-    });
-});
+const $li = $("li");
 
 function addInfo(event) {
     event.preventDefault();
-    let startingInput = $input.val();
-    let userInput = $ul.append(startingInput).append("<button id='remove'>x</button>");
+    let skillInput = $input.val();
+    $ul.append("<li></li>");
+    let userInput = $ul.append("<button id='remove'>x</button>").append(skillInput);
 };
+
+    $ul.on("click", "button", function() {
+        $(this).closest($ul).fadeOut(1000, function () {
+            $(this).remove();
+        })
+    })
 
 $btn.on("click", addInfo);
 $form.on("button", addInfo);
